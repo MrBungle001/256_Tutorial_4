912520d05f6471022218cd09bb071e278b979cf3
Kerry Fawdray <fawdrayk@gmail.com>
2021-08-09 20:07:44 +1000

    First commit

---
+++ src/main/java/Tutorial4/Calc.java
@@ -0,0 +1,15 @@
+package Tutorial4;
+
+public class Calc {
+
+	public int add(int intOne, int intTwo)
+	{
+		return intOne + intTwo;
+	}
+
+	public int subtract(int intOne, int intTwo)
+	{
+		return intOne + intTwo;
+	}
+		
+}
---
+++ src/test/java/Tutorial4/TestCalc.java
@@ -0,0 +1,13 @@
+package Tutorial4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestCalc {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		Calc cal = new Calc();
+		
+		assertTrue(cal.add(5, 5) == 10);
+	}
+}
